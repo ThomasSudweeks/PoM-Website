9382e4552088f6f7966d590dfc230203f1da2cd8
Thomas Sudweeks <thomassu@byu.edu>
2025-02-19 00:59:21 -0700

    PoM Logo Added in Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,9 @@ import './App.css'
 function App() {
   return (
     <>
-      {/* <div className="bg"></div> */}
+      <img src="./pom-dark.svg" alt="PoM Logo" className="icon-rim-dark"></img>
+      <img src="./pom-white.svg" alt="PoM Logo" className="icon-rim-light"></img>
+      <img src="./pom-green.svg" alt="PoM Logo" className="icon"></img>
       <div className="wrapper">
         <p className="title text" data-text="Peace of Mind">Peace of Mind</p>
         <p className="title-rim text">Peace of Mind</p>
